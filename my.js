1846368c8863e8fb20ff369ed74338bd26a10d54
habinbea <habinbea@handong.ac.kr>
2023-10-10 12:25:01 +0900

    added validateForm function

--- my.js
+++ my.js
@@ -1,10 +1,30 @@
 function edit_confirm() {
-  alert("Are you sure you want to save these changes?");
+  if (validateForm()) {
+    alert("Are you sure you want to save these changes?");
+  } else {
+    alert("Please add the TF name");
+  }
 }
 
 function delete_confirm() {
-  alert("Are you sure you want to delete this?");
+  if (validateForm()) {
+    alert("Are you sure you want to delete this TF?");
+  } else {
+    alert("Please add the TF name");
+  }
 }
 function add_confirm() {
-  alert("A new TF was added");
+  if (validateForm()) {
+    alert("A new TF was added");
+  } else {
+    alert("Please add the TF name");
+  }
+}
+function validateForm() {
+  var TF_name = document.getElementById("TF_name").value;
+  if (TF_name == "") {
+    return false;
+  } else {
+    return true;
+  }
 }

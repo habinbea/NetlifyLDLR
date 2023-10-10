f936dc82165cf1903867d8901528bac3eb6b1b5c
habinbea <habinbea@handong.ac.kr>
2023-10-10 12:07:20 +0900

    completed

---
+++ my.js
@@ -0,0 +1,10 @@
+function edit_confirm() {
+  alert("Are you sure you want to save these changes?");
+}
+
+function delete_confirm() {
+  alert("Are you sure you want to delete this?");
+}
+function add_confirm() {
+  alert("A new TF was added");
+}
